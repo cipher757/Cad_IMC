e88969edcd18554137e417a516ccb4ada2d784ee
João Otávio <jofs2006@gmail.com>
2024-03-05 15:09:07 -0300

    added more IMC info and visual changes

--- src/app/imc/imc.component.ts
+++ src/app/imc/imc.component.ts
@@ -9,24 +9,36 @@ export class ImcComponent {
   peso: number = 0;
   altura: number = 0;
   resultado: number = 0;
+  faixa: string = "";
 
-  calc() {
-      // Verifica se o peso e a altura são valores válidos (maiores que zero)
-      if (this.peso <= 0 || this.altura <= 0) {
-          console.error("O peso e a altura devem ser valores positivos.");
-          return;
-      }
+    calc() {
+        //se o peso e a altura são valores válidos (maiores que zero)
+        if (this.peso <= 0 || this.altura <= 0) {
+            console.error("O peso e a altura devem ser valores positivos.");
+            return;
+        }
 
-      // Calcula o IMC
-      this.resultado = this.peso / (this.altura * this.altura);
+        //calculo imc
+        this.resultado = this.peso / (this.altura * this.altura);
 
-      // arredonda o resultado para duas casas decimais
-      this.resultado = this.roundToTwoDecimalPlaces(this.resultado);
-  }
+        //faixa de IMC
+        if (this.resultado < 18.5) {
+            this.faixa = "Abaixo do peso";
+        } else if (this.resultado >= 18.5 && this.resultado < 25) {
+            this.faixa = "Peso normal";
+        } else if (this.resultado >= 25 && this.resultado < 30) {
+            this.faixa = "Sobrepeso";
+        } else {
+            this.faixa = "Obesidade";
+        }
 
-  // arredondar um número para duas casas decimais
-  roundToTwoDecimalPlaces(value: number): number {
-      return Math.round(value * 100) / 100;
-  }
+        //arredonda o resultado para duas casas decimais
+        this.resultado = this.roundToTwoDecimalPlaces(this.resultado);
+    }
+
+    //arredonda um número para duas casas decimais
+    roundToTwoDecimalPlaces(value: number): number {
+        return Math.round(value * 100) / 100;
+    }
 
 }
